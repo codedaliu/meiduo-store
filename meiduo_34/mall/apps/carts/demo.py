d990f75a8b2df452f2bb46360d5b743d1a82d0c3
codedaliu <49611@qq.com>
2019-01-26 15:19:46 +0800

    01

--- meiduo_34/mall/apps/carts/demo.py
+++
@@ -1,8 +0,0 @@
-#"_*_coding:UTF-8_*_"
-import pickle
-
-dict = {'name':'daliu'}
-dump = pickle.dumps(dict)
-print(dump)
-loda = pickle.loads(dump)
-print(loda)
--- meiduo_34/mall/apps/oauth/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from .views import OAuthQQURLAPIView,OAuthQQUserAPIView
-urlpatterns =[
-    url(r'^qq/statues/$',OAuthQQURLAPIView.as_view()),
-
-    url(r'^qq/users/$',OAuthQQUserAPIView.as_view()),
-]
